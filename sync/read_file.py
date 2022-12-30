1dc2ff3129a3a925948777b9bb4bd4c1a0735701
Duc Thinh <thinhapcs@gmail.com>
2022-12-30 15:59:41 +0700

    update threading lru cache

--- sync/read_file.py
+++ sync/read_file.py
@@ -4,8 +4,6 @@ file_path = 'data2/TH1.txt'
 def read_all_image_urls_from_file(path=file_path):
     urls = []
 
-    print(path)
-
     file = open(path, 'r')
     lines = file.readlines()
     file.close()
